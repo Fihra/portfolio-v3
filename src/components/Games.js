8fb3762c1da9ea4d3ba7458b6062ce7f2bba67fd
fihra <fabrofabian@gmail.com>
2023-11-09 23:58:46 -1000

    get games

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,4 +1,6 @@
 import React, {useEffect} from 'react';
+import GameCard from './GameCard';
+import data from "../data.json";
 
 const Games = () => {
     useEffect(() => {
@@ -6,11 +8,19 @@ const Games = () => {
         contentContainer.classList.add("show-content-border");
     }, [])
 
+    const showGames = () => {
+        return data.games.map((game, i) => {
+           return <GameCard key={i} game={game}/>
+        })
+    }
+
     return (
         <div id="content-container" className="games-container">
             <fieldset>
                 <legend>Games</legend>
-                <p>Games dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div className="all-games">
+                {showGames()}
+                </div>
             </fieldset>
         </div>
     );
