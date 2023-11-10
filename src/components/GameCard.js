53bf4b88c72a83e1d9d7a370e48c3876a338288c
fihra <fabrofabian@gmail.com>
2023-11-10 00:13:22 -1000

    setup css game card hover show name

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const GameCard = (props) => {
-    console.log(props);
+    const [hover, setHover] = useState(false);
     const { name, role, type, description, github, image, itchio, ost, video } = props.game;
 
-    console.log(image);
+    const onHover = () => {
+        console.log("hovered");
+        setHover(true);
+    }
+
+    const onHoverOver = () =>{
+        console.log("leave");
+        setHover(false);
+    }
 
     return(
         <div className="game-card">
-            <img src={image} width={180} height={180}/>
+            <img src={image} width={180} height={180} onMouseEnter={onHover} onMouseLeave={onHoverOver}/>
+            <div className="game-info">
+                <p>{name}</p>
+            </div>
         </div>
     )
 }
