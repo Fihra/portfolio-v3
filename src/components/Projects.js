4f69c568477f0e4e3870bb294276790c0b2a5be8
fihra <fabrofabian@gmail.com>
2023-11-10 23:33:00 -1000

    render each project card

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,6 @@
 import React, { useEffect } from 'react';
+import ProjectCard from './ProjectCard';
+import data from '../data.json';
 
 const Projects = () => {
     useEffect(() => {
@@ -6,11 +8,19 @@ const Projects = () => {
         contentContainer.classList.add("show-content-border");
     }, [])
 
+    const showProjects = () => {
+        return data.projects.map((project, i) => {
+            return <ProjectCard key={i} project={project}/>
+        })
+    }
+
     return (
         <div id="content-container" className="projects-container">
             <fieldset>
                 <legend>Projects</legend>
-                <p>Projects dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div className="all-projects">
+                    {showProjects()}
+                </div>
             </fieldset>
         </div>
     );
