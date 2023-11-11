9ebf568381ee74a3b9e14b96e37fac3959f467ea
fihra <fabrofabian@gmail.com>
2023-11-10 23:33:14 -1000

    setup modal and display for projects

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,9 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+import { FaGithub } from 'react-icons/fa';
+import { MdOutlineGames } from 'react-icons/md';
+import { FaMusic } from 'react-icons/fa';
 
-const ProjectCard = () => {
+const customStyle = {
+    content: {
+        zIndex: '3',
+        width: 500,
+        height: 600,
+        textAlign: 'center',
+        overflow: 'hidden',
+        margin: '0 auto',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)'
+    }
+}
+
+const ProjectCard = (props) => {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [hover, setHover] = useState(false);
+    const { name, image, type, description, itchio, github, link } = props.project;
+
+    const onHover = () => {
+        setHover(true);
+    }
+
+    const onHoverOver = () =>{
+        setHover(false);
+    }
+
+    const openModal = () => {
+        setModalIsOpen(true);
+    }
+
+    const closeModal = () => {
+        setModalIsOpen(false);
+    }
+
+    const iconSize = '24px';
+
     return(
-        <div>
-            
+        <div className="project-card">
+            <Modal
+                style={customStyle}
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+            >
+                <h2>{name}</h2>
+                <img src={image} width={470} height={250}/>
+                <h4>Type: {type}</h4>
+                <p>{description}</p>
+                <div className="icons-container">
+                    <ul>
+                        {(github !== "") && <li><a href={github}><FaGithub size={iconSize} color={"white"}/></a></li>}
+                        {(itchio !== "") && <li><a href={itchio}><MdOutlineGames size={iconSize} color={"white"}/></a></li> }
+                        {(link !== "") && <li><a href={link}><FaMusic size={iconSize} color={"white"}/></a></li>}                    
+                    </ul>
+                </div>
+            </Modal>
+            <img src={image} width={180} height={180} onClick={openModal} onMouseEnter={onHover} onMouseLeave={onHoverOver}/>
+            <div className="project-info">
+                <p>{name}</p>
+            </div>
         </div>
     )
 }
