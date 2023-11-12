b199e46a7695935a94a9191dba40ca9493a5c174
Fihra <fabrofabian@gmail.com>
2023-11-11 14:08:56 -1000

    refactored to use data.json for header roles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import data from '../data.json';
 
 const Header = () => {
+  const { header_roles } = data;
+
+
+  const showRoles = () => {
+    return header_roles.map((role, i) => {
+      if(i < header_roles.length -1){
+        return <>
+        <h2>{role}</h2><span className="header-dot">{'\u2B24'}</span>
+      </>
+      } else {
+        return(
+          <h2>{role}</h2>
+        ) 
+      }
+      
+    })
+  }
+
   return (
-    <div>
+    <div className="header-container">
       <h1>Fabian Fabro</h1>
-      <h2>Software Developer<span className="header-dot">{'\u2B24'}</span>Technical Audio Designer<span className="header-dot">{'\u2B24'}</span>Composer</h2>
+      <section className="header-roles-container">
+      {showRoles()}
+      </section>
     </div>
   );
 }
