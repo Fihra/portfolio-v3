179ed4cea53566e8faccbe1309083a5c42b0771b
fihra <fabrofabian@gmail.com>
2023-11-14 21:44:52 -1000

    created thank you message on form submit

---
+++ src/components/ThankYouMessageForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { IoMdCheckmarkCircle } from "react-icons/io";
+
+const ThankYouMessageForm = () => {
+    return(
+        <React.Fragment>
+            <h3>Thank you for your submission! <IoMdCheckmarkCircle/></h3>    
+        </React.Fragment>
+    )
+}
+
+export default ThankYouMessageForm;
