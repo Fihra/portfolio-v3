a2a432c12b695a3a6988ee9cd7fcf400835986e0
fihra <fabrofabian@gmail.com>
2024-06-15 12:18:49 -1000

    create start arrow click at start of home

---
+++ src/components/StartClick.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
+const StartClick = () => {
+    return(
+        <div className="start-click">
+            <div className="start-arrow"><ArrowBackIcon fontSize="inherit"/></div>
+            <h3>Click Letter</h3>
+        </div>
+    )
+}
+
+export default StartClick;
