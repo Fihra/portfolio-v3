9ae42a3f306ca13688af570de20f3f25ee05634e
fihra <fabrofabian@gmail.com>
2024-06-15 12:19:08 -1000

    hover on nodes show node names

--- src/components/NavNode.js
+++ src/components/NavNode.js
@@ -1,18 +1,29 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { MainContext } from '../context/MainContext';
 
 const NavNode = (props) => {
     const { setNode, currentNode } = useContext(MainContext);
     const { node } = props;
+    const [hover, setHover] = useState(false);
 
     const showNodeName = () => {
         setNode(node.toLowerCase());
         window.scrollTo(0, 0);
     }
 
+    const onHover = () => {
+        console.log(node)
+        setHover(true);
+    }
+
+    const onHoverOver = () =>{
+        setHover(false);
+    }
+
     return (
-        <div className={`node-circle ${node} ${node.toLowerCase() === currentNode ? "active-node" : ""}`}>
+        <div className={`node-circle ${node} ${node.toLowerCase() === currentNode ? "active-node" : ""}`} onMouseEnter={onHover} onMouseLeave={onHoverOver}>
             <p className="node-letter" onClick={showNodeName}>{node[0]}</p>
+            {hover && <div className="node-info"><p>{node.slice(1)}</p></div>}
         </div>
     );
 }
