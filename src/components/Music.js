7ed39eb9aebf5710638ce5815f5a2a8f3258029a
Fabian <fabrofabian@gmail.com>
2025-08-28 01:24:36 -0700

    added music website link

--- src/components/Music.js
+++ src/components/Music.js
@@ -4,7 +4,7 @@ import { FaSpotify } from 'react-icons/fa';
 import data from '../data.json';
 
 const Music = () => {
-    const { bandcamp, spotify } = data.music[0];
+    const { bandcamp, spotify, website } = data.music[0];
     useEffect(() => {
         let contentContainer = document.getElementById("content-container");
         contentContainer.classList.add("show-content-border");
@@ -18,7 +18,7 @@ const Music = () => {
                 <legend>Music</legend>
                 <p>Check out my music here</p>
                 <br/>
-                <p>Music Site coming soon</p>
+                <a href={website} target="_blank" rel="noreferrer">{website}</a>
                 <br/>
                 <ul className="music-links">
                     <li><a href={bandcamp}><FaBandcamp size={iconSize} color={"white"}/></a></li>
